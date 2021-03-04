152eb61805cca445e01cc216bf62e9b3119ff691
Git-DRL <76605251+Git-DRL@users.noreply.github.com>
2021-03-04 15:30:39 +0800

    Update TestServlet.java
    222

--- src/main/java/test/TestServlet.java
+++ src/main/java/test/TestServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 public class TestServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println(123465);
+		System.out.println(1111);
 		req.getRequestDispatcher("index.jsp").forward(req, resp);
 	}
 }
